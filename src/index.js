6895a048baa99b96b274cb96186b6708b0824cec
Saiket Das <saiketdas02@gmail.com>
2022-11-15 23:55:47 +0600

    Debugging Redux Applications

--- src/index.js
+++ src/index.js
@@ -1,38 +1,40 @@
 //  ------------- REDUX FUNDAMENTALS -------------
 
 import store from "./store/store";
-// import * as actions from "./store/actions";
+import * as actions from "./store/actions";
 
-// const unsubscribe = store.subscribe(() => {
-//   console.log("Store changed", store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed", store.getState());
+});
+
+store.dispatch(actions.bugAdded("Bug 1"));
+store.dispatch(actions.bugAdded("Bug 2"));
+store.dispatch(actions.bugAdded("Bug 3"));
+store.dispatch(actions.bugResolved(1));
+
+// unsubscribe();
+
+// store.dispatch({
+//   type: actions.BUG_REMOVED,
+//   payload: {
+//     id: 1,
+//   },
 // });
 
-// store.dispatch(actions.bugAdded("Bug 1"));
-// store.dispatch(actions.bugResolved(1));
-
-// // unsubscribe();
-
-// // store.dispatch({
-// //   type: actions.BUG_REMOVED,
-// //   payload: {
-// //     id: 1,
-// //   },
-// // });
-
-// console.log("Current state", store.getState());
+console.log("Current state", store.getState());
 
 //  ------------- BUILDING REDUX FROM SCRATCH -------------
 
 // import store from "./store/customStore";
-import * as actions from "./store/actions";
+// import * as actions from "./store/actions";
 
-// Notify user that what chnaged
-store.subscribe(() => {
-  console.log("Store changed", store.getState());
-});
+// // Notify user that what chnaged
+// store.subscribe(() => {
+//   console.log("Store changed", store.getState());
+// });
 
-// Changing state
-store.dispatch(actions.bugAdded("Bug 1"));
+// // Changing state
+// store.dispatch(actions.bugAdded("Bug 1"));
 
-// Get current state
-console.log("Current state", store.getState());
+// // Get current state
+// console.log("Current state", store.getState());
