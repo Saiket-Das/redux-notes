56410584598010bf3c0b703eddf2f9a353ce6ede
Saiket Das <saiketdas02@gmail.com>
2022-11-15 23:03:37 +0600

    Building Redux from Scratch

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
-import store from "./store/store";
-import { bugAdded, bugResolved } from "./store/actions";
+//  ------------- REDUX FUNDAMENTALS -------------
 
-const unsubscribe = store.subscribe(() => {
+// import store from "./store/store";
+// import * as actions from "./store/actions";
+
+// const unsubscribe = store.subscribe(() => {
+//   console.log("Store changed", store.getState());
+// });
+
+// store.dispatch(actions.bugAdded("Bug 1"));
+// store.dispatch(actions.bugResolved(1));
+
+// // unsubscribe();
+
+// // store.dispatch({
+// //   type: actions.BUG_REMOVED,
+// //   payload: {
+// //     id: 1,
+// //   },
+// // });
+
+// console.log("Current state", store.getState());
+
+//  ------------- BUILDING REDUX FROM SCRATCH -------------
+
+import store from "./store/customStore";
+import * as actions from "./store/actions";
+
+// Notify user that what chnaged
+store.subscribe(() => {
   console.log("Store changed", store.getState());
 });
 
-store.dispatch(bugAdded("Bug 1"));
-store.dispatch(bugResolved(1));
+// Changing state
+store.dispatch(actions.bugAdded("Bug 1"));
 
-// unsubscribe();
-
-// store.dispatch({
-//   type: actions.BUG_REMOVED,
-//   payload: {
-//     id: 1,
-//   },
-// });
-
+// Get current state
 console.log("Current state", store.getState());
