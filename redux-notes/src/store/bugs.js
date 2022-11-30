2935152ff7587963ed760352bbbbc4a97ebdfc53
Saiket Das <saiketdas02@gmail.com>
2022-11-30 14:34:06 +0600

    Redux Notes - Intialstate of bugs updated

--- redux-notes/src/store/bugs.js
+++ redux-notes/src/store/bugs.js
@@ -7,11 +7,15 @@ let lastId = 0;
 // Combine Action creator & Reducer
 const slice = createSlice({
   name: "bugs",
-  initialState: [],
+  initialState: {
+    list: [],
+    loading: false,
+    lastFetch: null,
+  },
   reducers: {
     // actions => actions handler
     bugAdded: (bugs, action) => {
-      bugs.push({
+      bugs.list.push({
         id: ++lastId,
         description: action.payload.description,
         // userId =
@@ -20,14 +24,14 @@ const slice = createSlice({
     },
 
     bugResolved: (bugs, action) => {
-      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-      bugs[index].resolved = true;
+      const index = bugs.list.findIndex((bug) => bug.id === action.payload.id);
+      bugs.list[index].resolved = true;
     },
 
     bugAssignToUser: (bugs, action) => {
       const { bugId, userId } = action.payload;
-      const index = bugs.findIndex((bug) => bug.id === bugId);
-      bugs[index].userId = userId;
+      const index = bugs.list.findIndex((bug) => bug.id === bugId);
+      bugs.list[index].userId = userId;
     },
   },
 });
