5c60136643ae2f9de9e600706016d8edc96996f8
Saiket Das <saiketdas02@gmail.com>
2022-12-24 23:40:33 +0600

    Moon Tech - React icons updated in Frontend

--- moon-tech/frontend/src/layout/Dashboard/Sidebar.js
+++ moon-tech/frontend/src/layout/Dashboard/Sidebar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { IoIosArrowBack } from "react-icons/io";
+
 const Sidebar = () => {
   return (
     <div className="col-span-2 bg-indigo-200 h-[calc(100vh-25px)] p-5 rounded-lg">
@@ -12,7 +14,8 @@ const Sidebar = () => {
         <li>
           <Link to="add-product"> Add Product </Link>
         </li>
-        <li className="mt-auto">
+        <li className="flex items-center mt-auto">
+          <IoIosArrowBack className="mr-1" />
           <Link to="/">Back to Home </Link>
         </li>
       </ul>
