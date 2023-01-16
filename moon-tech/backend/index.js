1d47ee1e37f09da3355b68ecdc0c4ec576fc8a18
Saiket Das <saiketdas02@gmail.com>
2023-01-16 23:30:05 +0600

    Moon Tech - Backend's root api updated

--- moon-tech/backend/index.js
+++ moon-tech/backend/index.js
@@ -49,7 +49,7 @@ const run = async () => {
 run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Moon Tech is runing!");
+  res.send("Moon Tech is running!");
 });
 
 app.listen(port, () => {
